49d6dd68fc3572cab8ebcc21297a1ebcdf19adbe
Jamie <jxberc@gmail.com>
2014-08-09 18:46:27 -0400

    add email to form during token registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,21 @@
 # app/controllers/registrations_controller.rb
 class RegistrationsController < Devise::RegistrationsController
 
-  def new
+  def new  
+    build_resource({})  
     @token = params[:invite_token] #<-- pulls the value from the url query string
-    super 
+    if @token
+      @invite = Invite.find_by_token(@token)
+      resource.email = @invite.email if @invite
+    end
+    respond_with self.resource
+
   end
 
   def create
     super
     @token = params[:invite_token] #<-- pulls the value from the url query string
-    if @token != nil
+    if @token
         @invite = Invite.find_by_token(@token)
         @invite.update_attributes(:recipient => resource)
     end
