dd732fc4dcff825059817a35f3b9a338a1f824a1
Jamie <jxberc@gmail.com>
2014-08-09 20:56:25 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,17 @@ User.destroy_all
 Vote.destroy_all
 Invite.destroy_all
 
-User.create!(email: "jeb@email.com", password: "watermellon")
-User.create!(email: "jane@email.com", password: "watermellon")
-User.create!(email: "jimbo@email.com", password: "watermellon")
+u = User.create!(email: "jamie@email.com", password: "watermellon")
+User.create!(email: "liam@email.com", password: "watermellon")
+User.create!(email: "dad@email.com", password: "watermellon")
+User.create!(email: "mom@email.com", password: "watermellon")
+
+Vote.create!(question: "Favorite fruit", choices: "apple\r\nbanana\r\norange", creator_id:1)
+Vote.create!(question: "Favorite drink", choices: "soda\r\njuice\r\nwater", creator_id:2)
+
+Invite.create!(email: 'liam@email.com', sender_id: 1, recipient_id: 2, vote_id: 1)
+Invite.create!(email: 'dad@email.com', sender_id: 1, recipient_id: 3, vote_id: 1)
+Invite.create!(email: 'mom@email.com', sender_id: 1, recipient_id: 4, vote_id: 1)
+
+
 
