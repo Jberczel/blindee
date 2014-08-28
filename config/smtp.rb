a876f34f6c6e48ff44167b0dd3e614cd0cc4e40d
Jamie <jxberc@gmail.com>
2014-08-28 17:15:01 -0400

    edit another typo

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,9 +1,9 @@
 SMTP_SETTINGS = {
-  address: 'smtp.mandrillapp.com', # example: "smtp.sendgrid.net"
+  address: 'smtp.mandrillapp.com',, # example: "smtp.sendgrid.net"
   authentication: :plain,
   domain: 'heroku.com', # example: "this-app.com"
   enable_starttls_auto: true,
-  password: ENV.fetch('MANDRILL_APIKEY'),
+  password: ENV['MANDRILL_APIKEY'],
   port: "587",
-  user_name: ENV.fetch['MANDRILL_USERNAME']
+  user_name: ENV['MANDRILL_USERNAME']
 }
