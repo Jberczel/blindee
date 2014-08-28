6c883925e270a1a7406bedf2ffb797afe146e949
Jamie <jxberc@gmail.com>
2014-08-28 16:08:40 -0400

    add choice validation and finished? helper

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -8,11 +8,15 @@ class Vote < ActiveRecord::Base
   before_save :add_quote
 
   def get_choices
-    choices.split(/\r?\n/)
+    choices.split(/\r?\n/).select { |c| c =~ /\S+/ }
   end
 
   def get_participation_rate
     answers.count.to_f / (invites.count + 1)  # add creator to count
+  end
+
+  def finished?
+    get_participation_rate > 0.75
   end
 
   private
