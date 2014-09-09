a9cddc942dfe9d4088a514b1077350c03a5a45c2
Jamie <jxberc@gmail.com>
2014-09-09 09:14:04 -0400

    add device homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @created_votes = current_user.created_votes
-    @invitations = current_user.invitations
+    if user_signed_in?
+      @created_votes = current_user.created_votes
+      @invitations = current_user.invitations
+    end
   end
 
   def about
