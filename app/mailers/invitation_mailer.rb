18e7363eac386e3ff61d23cf3624b36ed1ed9e2d
Jamie <jxberc@gmail.com>
2014-09-10 09:34:36 -0400

    edit email sender

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,5 +1,5 @@
 class InvitationMailer < MandrillMailer::TemplateMailer
-  default from: "contact@blindee.com", from_name: "Blindee"
+  default from: "no-reply@blindee.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
