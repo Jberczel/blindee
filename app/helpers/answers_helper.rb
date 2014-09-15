998a5862f844124583ba9d76fb36fefae1c3b6ee
Jamie <jxberc@gmail.com>
2014-09-15 10:50:24 -0400

    fix percentage formula

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,7 +1,7 @@
 module AnswersHelper
 
   def participation_rate    
-    num = @answers.count / (@vote.invites.count + 1) * 100
+    num = (@answers.count / (@vote.invites.count + 1.0)) * 100
     number_to_percentage(num, precision: 0)
   end
 end
