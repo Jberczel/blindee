877071183f0ae87a26f0c33c30906c6f89ac6f9d
Jamie <jxberc@gmail.com>
2014-09-23 14:17:27 -0400

    add truncated labels to chart

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -4,4 +4,9 @@ module AnswersHelper
     num = (@answers.count / (@vote.invites.count + 1.0)) * 100
     number_to_percentage(num, precision: 0)
   end
+
+  # truncated answers so that chart displays
+  def truncate(array)
+    array.map { |word| word.length < 15 ? word : "#{word[0..14]}..." }
+  end
 end
