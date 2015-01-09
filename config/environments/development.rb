b9b5c81bdc10ad5945fa21a9c95dc14411b89d89
Jamie <jxberc@gmail.com>
2015-01-09 16:28:03 -0500

    add bullet gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end
