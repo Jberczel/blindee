7ed26aeab1e341118337d9cee89c9af990d5de3b
Jamie <jxberc@gmail.com>
2015-01-15 11:51:45 -0500

    refactor truncate helper method

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -6,7 +6,7 @@ module AnswersHelper
   end
 
   # truncated answers so that chart displays
-  def truncate(array)
-    array.map { |word| word.length < 15 ? word : "#{word[0..14]}..." }
+  def truncate(ary, len=15)
+    ary.map { |w| w.length < len ? w : "#{w[0..len-1]}..." }
   end
 end
