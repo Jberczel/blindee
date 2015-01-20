d370c95864ba146ce0d186b2779d93de3667abbf
Jamie <jxberc@gmail.com>
2015-01-20 17:15:16 -0500

    refactor views to use presenter object

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
-      @created_votes = current_user.created_votes
-      @invitations = current_user.invitations
+      created_votes     = current_user.created_votes.includes(:creator)
+      invited_votes     = current_user.invitations.includes(:vote).map(&:vote)
+
+      @votes_presenter   = VotePresenter.wrap(created_votes, view_context)
+      @invites_presenter = VotePresenter.wrap(invited_votes, view_context)
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   def markdown(content)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,:hard_wrap=>true,:autolink=>true,:no_intra_emphasis=>true)
     markdown.render(h(content)).html_safe
